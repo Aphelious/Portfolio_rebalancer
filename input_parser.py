bc9ed88e9cbe008154bd76cf8a850c2636ddafa5
Mike Powers <72277454+Aphelious@users.noreply.github.com>
2022-03-22 16:14:13 -0400

    Add Todo list for full input data checking

--- input_parser.py
+++ input_parser.py
@@ -1,11 +1,28 @@
 from env import *
 import csv
 from datetime import datetime
+from models import Transaction
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
-engine = create_engine(db_file)
+engine = create_engine(f'sqlite:///transactions.db', echo=False, future=True)
 session = Session(engine)
+
+# Steps for parsing and interpreting csv/any input:
+
+# Check the file format/extension
+# Check file contents?
+# Determine Delimiter
+# Determine line break: CR, LF, or CRLF
+# Determine encoding
+# Determine UTF character at start or not
+# Open and read in contents
+# Check if a header exists
+# Check if missing data
+# Handle rows of missing data
+# Assign data type to column based on header info?
+# Check if Column data matches header data type
+
 
 # Read rows directly from cvs into kwarg:value pairs, then to the session to be instantiated and added to db using
 # Sqlalchemy orm:
